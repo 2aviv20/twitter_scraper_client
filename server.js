0d72a10af34ef276f4a86acd802cbf2006ef7cf1
2aviv20 <2aviv20@gmail.com>
2018-11-27 14:12:41 +0200

    fix server

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
+app.use(express.static(__dirname + '/dist/cycurity-client/'));
 app.get('/*', (req, res) => res.sendfile(path.join(__dirname + '/dist/cycurity-client/index.html')));
-app.listen(process.env.PORT || 8080, () => console.log("server is running !!"));
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log("server is running !!"+port));
